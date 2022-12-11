4fc1db1bb8748cde5f712f24429841e7800f85d6
Vitaly Borodulin <rainbowf@mail.ru>
2022-12-12 00:22:24 +0500

    Fix bug with entries displaying

--- beauty_salon/views.py
+++ beauty_salon/views.py
@@ -68,22 +68,15 @@ def adm(request):
 @login_required
 def notes(request):
     current_date = datetime.now().date()
-    current_time = datetime.now().time()
     entries = Entry.objects.filter(client=request.user)
-    past_entries = (
-        entries
-        .filter(time_point__date__lte=current_date)
-        .filter(time_point__time__lte=current_time)
-    )
-    future_entries = (
-        entries
-        .filter(time_point__date__gt=current_date)
-        .filter(time_point__time__gt=current_time)
-    )
+    past_entries = entries.filter(time_point__date__lt=current_date)
+    future_entries = entries.filter(time_point__date__gt=current_date)
+    current_entries = entries.filter(time_point__time__eq=current_date)
     debt = entries.filter(status='not_payed').aggregate(Sum('service__price'))
     context = {
         'past_entries': past_entries,
         'future_entries': future_entries,
+        'current_entries': current_entries,
         'debt': debt
     }
     if request.method == 'POST':
