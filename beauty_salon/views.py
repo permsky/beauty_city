bf69672959c8ac38439a4937105a52cf2ef0c532
b10t <b10t.rus@gmail.com>
2022-12-12 21:05:00 +0700

    Output of data on salons from the database.

--- beauty_salon/views.py
+++ beauty_salon/views.py
@@ -126,9 +126,7 @@ def service_finally(request):
         prof = service.get('prof')
         service = service.get('service')
 
-        salon = salon.split(' ', maxsplit=2)
-        salon.pop(2)
-        salon = ' '.join(salon)
+        salon = salon.split('#', maxsplit=1)[0]
         salon = Salon.objects.get(name=salon)
 
         master = Master.objects.get(name=master)
